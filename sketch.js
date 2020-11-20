36d87b5c89624dfae5135b9f48bab60a1b7cba63
Suchismita2008 <74309204+Suchismita2008@users.noreply.github.com>
2020-11-20 19:53:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixed_rect,moving_rect;
+ function setup()
+  {
+     createCanvas(1200,800);
+      fixed_rect= createSprite(600,400,50,80);
+       fixed_rect.shapeColor="blue";
+        fixed_rect.debug=true;
+        
+        moving_rect= createSprite(400,200,80,50);
+         moving_rect.shapeColor="blue";
+          moving_rect.debug=true; 
+  } 
+        function draw()
+         {
+            background("lightgreen");
+            moving_rect.x=World.mouseX;
+              moving_rect.y=World.mouseY;
+               if(moving_rect.x-fixed_rect.x < moving_rect.width/2+fixed_rect.width/2
+                 && fixed_rect.x-moving_rect.x<moving_rect.width/2+fixed_rect.width/2
+                 && moving_rect.y-fixed_rect.y < moving_rect.height/2+fixed_rect.height/2
+                 && fixed_rect.y-moving_rect.y<moving_rect.height/2+fixed_rect.height/2) 
+                 { 
+                   fixed_rect.shapeColor="red";
+                    moving_rect.shapeColor="red";
+                 }
+                  else 
+                  {
+                     fixed_rect.shapeColor="black";
+                      moving_rect.shapeColor="black";
+                     } 
+                     drawSprites();
+   }
